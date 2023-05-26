4da0b266d410646479b1b5023c360f9ce2b0fffd
qureshikaif <qureshik488@gmail.com>
2023-05-26 13:42:45 +0500

    Modified files

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+class Customer {
+    private String name;
+    private String contact;
+    private String cart;
+}
+class OnlineCustomer {
+
+}
+class InStoreCustomer {
+
+}
+class Items {
+
+}
+class SalesPerson {
+
+}
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
