e1db3d30ce5daa2b06f9128abf6cf361f2c4d8b0
qureshikaif <qureshik488@gmail.com>
2023-05-26 13:57:23 +0500

    Modified main file

--- src/App.java
+++ src/App.java
@@ -10,20 +10,30 @@ class Customer {
 
     }
     public void updateQuantity() {
+
+    }
+    public void generateBill () {
         
     }
 }
 class OnlineCustomer extends Customer {
-
+    private String address;
 }
 class InStoreCustomer extends Customer {
     private String rewardPoints;
 }
 class Items {
+    private String itemName;
+    private String quantity;
+    private double unitPrice;
 
 }
 class SalesPerson {
+    private String salesPersonId;
 
+    public void caclulateBill () {
+
+    }
 }
 public class App {
     public static void main(String[] args) throws Exception {
