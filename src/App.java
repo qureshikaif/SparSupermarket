246523db2c3a5e025d6746960216367b429b15a5
qureshikaif <qureshik488@gmail.com>
2023-05-28 17:09:47 +0500

    Modified App.java

--- src/App.java
+++ src/App.java
@@ -88,6 +88,13 @@ class Inventory {
     public void removeItem(Items item) {
         items.remove(item);
     }
+    public void showItems() {
+        for (Items item : items) {
+            System.out.println("Item Name: " + item.getItemName() +
+                    ", Quantity: " + item.getQuantity() +
+                    ", Unit Price: " + item.getUnitPrice());
+        }
+    }
 
     public void checkItemQuantity(Items item) {
         int quantity = item.getQuantity();
@@ -99,12 +106,24 @@ class Inventory {
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Inventory inventory = new Inventory();
+        Items rice = new Items("Rice", 100, 400);
+        Items flour = new Items("Flour", 100, 150);
+        Items egg = new Items("Egg", 100, 25);
+        Items meat = new Items("Meat", 100, 650);
+        Items bread = new Items("Bread", 100, 80);
+        inventory.addItem(rice);
+        inventory.addItem(flour);
+        inventory.addItem(egg);
+        inventory.addItem(meat);
+        inventory.addItem(bread);
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Spar Supermarket");
         System.out.println("1) Login as an admin\n2) Login as a customer\n3) Login as a Salesperson");
         int userChoice = input.nextInt();
         if(userChoice==1) {
             System.out.println("Press 1 to show inventory\nPress 2 to add item\nPress 3 to show number of online customers\nPress 4 to show number of Instore customers");
+            inventory.showItems();
 
         }
         else if(userChoice==2) {
