af9c12e15bee0d84430fd3def4b28fb0d25cd5a5
qureshikaif <qureshik488@gmail.com>
2023-06-02 20:06:17 +0500

    Optimized the code for better performance

--- src/InStoreCustomer.java
+++ src/InStoreCustomer.java
@@ -3,6 +3,19 @@ public class InStoreCustomer extends Customer {
     public void rewardPoints (int rewardPoints) {
         this.rewardPoints = rewardPoints;
     }
-    
-
+    public int getRewardPoints () {
+        return rewardPoints;
+    }
+    public double rewardPointsToDiscount (double totalBill) {
+        if(this.rewardPoints == 10) {
+            return totalBill * 0.02;
+        }
+        else if(this.rewardPoints > 10 || this.rewardPoints <=30) {
+            return totalBill * 0.06;
+        }
+        else if(this.rewardPoints > 30) {
+            return totalBill * 0.08;
+        }
+        return 0.0;
+    }
 }
--- src/SalesPerson.java
+++ src/SalesPerson.java
@@ -1,20 +1,30 @@
+import java.util.Scanner;
 public class SalesPerson {
+    Scanner input = new Scanner(System.in);
     private String salesPersonId="E5573-8561";
     private double totalBill;
     private double totalBillGst;
     private int totalInstoreCustomers;
+    InStoreCustomer instoreCustomer = new InStoreCustomer();
     public void generateBill(String instoreUname,String instoreCnic,String itemCart,int itemQuantity, double unitPrice) {
         totalBill = itemQuantity*unitPrice;
         totalBillGst = totalBill*0.15;
         totalBill = totalBillGst + totalBill;
-
         System.out.println("Your bill receipt: ");
         System.out.println("Salesperson ID: " + salesPersonId);
         System.out.println("Customer name: " + instoreUname);
         System.out.println("Customer CNIC: " + instoreCnic);
         System.out.println("Item: " + itemCart);
         System.out.println("Item Quantity: " + itemQuantity);
-        System.out.println("Your total bill is " + totalBill + " including GST");
+        System.out.println("You have " + instoreCustomer.getRewardPoints() + " reward points. Do you want to avail them now?\nPress Y to avail the discount\nPress N to cancel");
+        String userChoice = input.next();
+        if(userChoice.equals("Y") || userChoice.equals("y")) {
+            System.out.println("Reward points availed. Your final bill after applying discount is " + instoreCustomer.rewardPointsToDiscount(totalBill));
+        }
+        else {
+            System.out.println("No discount availed.");
+            System.out.println("Your total bill is " + totalBill + " including GST");
+        }
         totalInstoreCustomers++;
     }
     public int getTotalInstoreCustomers () {
